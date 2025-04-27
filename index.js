8416e0cc7b81cefdb36bee825fcf80a766dbeb32
rwb-mp <icebluecamaro@gmail.com>
2025-04-27 18:02:24 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -509,9 +509,9 @@ document.getElementById("dlls").addEventListener("click", () => {
     day: 'Sun',
     date: '27',
     opp: 'at SF <img src="logos/giants.gif" width="20px" height="20px" style="vertical-align:bottom">',
-    time: '3:05',
-    score: '',
-    recap: 'Victory+'
+    time: '',
+    score: 'L 3-2',
+    recap: '<a href = "journal.html#apr27" target = "_blank">Recap</a>'
     },
     {
     id: '44',
@@ -2805,7 +2805,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("April 27 2025 15:05:00").getTime();
+const targetDate = new Date("April 28 2025 19:05:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
