f00e7ede8c5b2afff9ab127ff8e70ee7f36f114c
rwb-mp <icebluecamaro@gmail.com>
2025-05-05 17:33:20 -0500

    Add files via upload

--- journal.js
+++ journal.js
@@ -102,14 +102,14 @@ const schedule = [
   date: 'April 12',
   opp: 'at Seattle',
   score: 'L 9-2',
-  recap: `Kumar Rocker (0-1, 7.88) vs. Bryan Woo (1-0, 4.50). The Rangers' best opportunity came early when their first 4 batters reached, scoring a run and loading the bases. But Pederson flew out to RF and Jung and Burger struck out leaving the bases full. In the middle innings Woo retired 14 straight. Rocker's bad inning was the 3rd in which he gave up a walk, 2 doubles, and a triple. Caleb Boushley made his Rangers debut in relief of Rocker and gave up 5 runs over 3-2/3. The Rangers lost the division lead for the first time since April 1.`
+  recap: `Kumar Rocker (0-1, 7.88) vs. Bryan Woo (1-0, 4.50). The Rangers' best opportunity came in the 1st inning when their first 4 batters reached, scoring a run and loading the bases. But Pederson flew out to RF and Jung and Burger struck out leaving the bases full. In the middle innings Woo retired 14 straight. Rocker's bad inning was the 3rd in which he gave up a walk, 2 doubles, and a triple. Caleb Boushley made his Rangers debut in relief of Rocker and gave up 5 runs over 3-2/3. The Rangers lost the division lead for the first time since April 1.`
   },   
   {
   id: 'apr13',
   date: 'April 13',
   opp: 'at Seattle',
   score: 'L 3-1',
-  recap: `Nathan Eovaldi (1-1, 2.29) vs. Logan Gilbert (0-1, 2.55), two of the best in the AL. The Rangers could only manage 4 hits off Mariners pitching and wasted a quality start by Eovaldi. Each starting pitcher went 5 innings and gave up a HR, but Seattle's came with a baserunner. In the 6th a Julio Rodriquez single, an error by Jake Burger at 1B, and 2 hit-batters gave Seattle a 3-1 lead and ended Eovaldi's day. Hoby Milner pitched out of the bases-loaded-no-out jam. Four Mariners relievers held the Rangers to a single over the last 4 innings and Seattle completed the sweep. In the 5th inning the Mariners Miles Mastrobuoni was thrown out by Kyle Higashioka attempting to steal 2B. It was the first base-stealer thrown out by a Ranger catcher this season after 28 successful steals.`
+  recap: `Nathan Eovaldi (1-1, 2.29) vs. Logan Gilbert (0-1, 2.55), two of the best in the AL. The Rangers could only manage 4 hits off Mariners pitching and wasted a quality start by Eovaldi. Each starting pitcher went 5 innings and gave up a HR, but Seattle's came with a baserunner. In the 6th a Julio Rodriquez single, an error by Jake Burger at 1B, and 2 hit-batters gave Seattle a 3-1 lead and ended Eovaldi's day. Hoby Milner pitched out of the bases-loaded-no-out jam. 4 Mariners' relievers held the Rangers to a single over the last 4 innings and Seattle completed the sweep. In the 5th inning the Mariners Miles Mastrobuoni was thrown out by Kyle Higashioka attempting to steal 2B. It was the first base-stealer thrown out by a Ranger catcher this season after 28 successful steals.`
   },
   {
   id: 'apr15',
@@ -130,14 +130,14 @@ const schedule = [
   date: 'April 17',
   opp: 'LA Angels',
   score: 'W 5-3',
-  recap: `Kumar Rocker (0-2, 7.94) vs. Jack Kochanowicz (1-1, 5.74). The Rangers began the game with the best starting pitcher ERA in the AL. The teams traded runs in the 1st. In the 2nd the Rangers loaded the bases with nobody out for Taveras who delivered with a 2-run double. Garcia homered in the 3rd to make it 4-1. In the 5th with 2 out and 2 on, Jake Burger set up way off the bag allowing the baserunner a huge lead. Instead of throwing to 1B after fielding a hard grounder, Josh Jung threw to 2B and was late. Rengifo then plated 2 with a single to left. Seager led off the 7th with a 427-ft. HR to CF to extend the lead to 5-3. It was Seager's 100th as a Ranger. Rocker was sensational in his 7 innings, striking out 8 and walking none on just 78 pitches, 58 for strikes. Chris Martin threw a perfect 8th, and Robert Garcia threw a perfect 9th for his first career save.`
+  recap: `Kumar Rocker (0-2, 7.94) vs. Jack Kochanowicz (1-1, 5.74). The Rangers began the game with the best starting pitcher ERA in the AL. The teams traded runs in the 1st. In the 2nd the Rangers loaded the bases with nobody out for Taveras who delivered with a 2-run double. Garcia homered in the 3rd to make it 4-1. LA scored twice in the 5th, but Seager led off the 7th with a 427-foot HR to CF to extend the lead to 5-3. It was Seager's 100th as a Ranger. Rocker was sensational in his 7 innings, striking out 8 and walking none on just 78 pitches, 58 for strikes. Chris Martin threw a perfect 8th, and Robert Garcia threw a perfect 9th for his first career save.`
   },
   {
   id: 'apr18',
   date: 'April 18',
   opp: 'LA Dodgers',
   score: 'L 3-0',
-  recap: `Jacob deGrom (0-0, 4.30) vs. Yoshinobu Yamamoto (2-1, 1.23). The Rangers wasted a beautiful start by deGrom: 3 hits and a walk in 7 innings with 7 K's. The Dodgers opened the scoring when Tommy Edman hit deGrom's 2nd pitch of the game 398-ft. over the RF wall. The Rangers had their chances but couldn't answer. In the 2nd inning Jung doubled with 1 out, but Dustin Harris and Jake Burger left him at 2nd. With 2 outs in the 3rd, Semien and Seager singled but Pederson grounded to 1st to end the inning. In the 4th Dustin Harris doubled down the line in left with 2 outs, but Burger struck out to strand him. The Dodgers scored two runs in the 9th off the bullpen. Rangers' hitters whiffed 22 times, 15 on chases out of the strike zone.`
+  recap: `Jacob deGrom (0-0, 4.30) vs. Yoshinobu Yamamoto (2-1, 1.23). The Rangers wasted a beautiful start by deGrom: 3 hits and a walk in 7 innings with 7 K's. The Dodgers opened the scoring when Tommy Edman hit deGrom's 2nd pitch of the game 398-feet over the RF wall. The Rangers had their chances but couldn't answer. In the 2nd inning Jung doubled with 1 out, but Dustin Harris and Jake Burger left him at 2nd. With 2 outs in the 3rd, Semien and Seager singled but Pederson grounded to 1st to end the inning. With 2 outs in the 4th Dustin Harris doubled down the line in left, but Burger struck out to strand him. The Dodgers scored two runs in the 9th off the bullpen. Rangers' hitters whiffed 22 times, 15 on chases out of the strike zone.`
   },
   {
   id: 'apr19',
