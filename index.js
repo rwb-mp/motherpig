88e65f8a37420e5f2bd9aa7e489f4935a8adf556
rwb-mp <icebluecamaro@gmail.com>
2025-08-21 10:04:48 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2005,9 +2005,9 @@ document.getElementById("dlls").addEventListener("click", () => {
     day: 'Wed',
     date: '20',
     opp: 'at KC <img src="logos/royals.gif" width="20px" height="20px" style="vertical-align:bottom">',
-    time: '6:40',
-    score: '',
-    recap: 'Victory+'
+    time: '',
+    score: 'W 6-3',
+    recap: '<a href = "journal.html#aug20" target = "_blank">Recap</a>'
     },
     {
     id: '180',
@@ -2805,7 +2805,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("August 20 2025 18:40:00").getTime();
+const targetDate = new Date("August 21 2025 13:10:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
--- journal.js
+++ journal.js
@@ -895,6 +895,13 @@ const schedule = [
   score: 'L 5-2',
   recap: `Merrill Kelly (9-7, 3.36) vs. Seth Lugo (8-6, 3.77). Texas faced a starter who had an 8.84 ERA in his last 4 starts, but could muster only 4 base hits in the game. The Rangers' runs came on solo homers by Seager and Pederson. Kelly allowed only 2 baserunners in his 6 innings, but they were both solo home runs. Tied 2-2 in the 7th, Milner faced 3 batters and gave up 3 hits to load the bases. Winn replaced Milner and pitched out of the jam, allowing only one run, but the Royals put the game away in the 8th when Winn surrendered a 2-run home run.`
   },
+  {
+  id: 'aug20',
+  date: 'August 20',
+  opp: 'at Kansas City',
+  score: 'W 6-3',
+  recap: `Caleb Boushley (0-0, 4.91) vs. Noah Cameron (7-5, 2.47). The first 3 batters that Boushley faced went single, single, home run. Boushley got out of bases-loaded trouble in the 2nd, and then pitched a clean 3rd. The bullpen took over in the 4th and pitched 6 innings of 3-hit shutout ball. Langford began the game with 406-foot HR to left. Texas scored again in the 3rd on singles by Langford and Semien. Heim doubled in the 7th and scored on a Seager single to tie the score at 3-3. In the top of the ninth with 2 outs, Langford walked, Seager reached on an error, and Semien reached on a squibber in the infield to load the bases. Higashioka delivered with a double that scored all three runners. Langford reached base in all 5 of his plate appearances batting in the leadoff spot.`
+  },
 ]
 
 let html = '';
