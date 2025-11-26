bd5aee82e87f590fdbe45233ed73b7ee4e691226
rwb-mp <icebluecamaro@gmail.com>
2025-11-26 15:10:21 -0600

    Add files via upload

--- journal.js
+++ journal.js
@@ -3,84 +3,84 @@ const schedule = [
   id: 'mar26',
   date: 'March 26',
   opp: 'at Philadelphia',
-  score: '',
+  score: '2:05',
   recap: ``
   },
   {
   id: 'mar28',
   date: 'March 28',
   opp: 'at Philadelphia',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'mar29',
   date: 'March 29',
   opp: 'at Philadelphia',
-  score: '',
+  score: '12:35',
   recap: ``
   },
   {
   id: 'mar30',
   date: 'March 30',
   opp: 'at Baltimore',
-  score: '',
+  score: '5:35',
   recap: ``
   },
   {
   id: 'mar31',
   date: 'March 31',
   opp: 'at Baltimore',
-  score: '',
+  score: '5:35',
   recap: ``
   },
   {
   id: 'apr1',
   date: 'April 1',
   opp: 'at Baltimore',
-  score: '',
+  score: '11:35',
   recap: ``
   },
   {
   id: 'apr3',
   date: 'April 3',
   opp: 'Cincinnati',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'apr4',
   date: 'April 4',
   opp: 'Cincinnati',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'apr5',
   date: 'April 5',
   opp: 'Cincinnati',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'apr6',
   date: 'April 6',
   opp: 'Seattle',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr7',
   date: 'April 7',
   opp: 'Seattle',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr8',
   date: 'April 8',
   opp: 'Seattle',
-  score: '',
+  score: '7:05',
   recap: ``
   },  
   {
@@ -108,196 +108,196 @@ const schedule = [
   id: 'apr13',
   date: 'April 13',
   opp: 'at Athletics',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'apr14',
   date: 'April 14',
   opp: 'at Athletics',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'apr15',
   date: 'April 15',
   opp: 'at Athletics',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'apr16',
   date: 'April 16',
   opp: 'at Athletics',
-  score: '',
+  score: '2"05',
   recap: ``
   },
   {
   id: 'apr17',
   date: 'April 17',
   opp: 'at Seattle',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'apr18',
   date: 'April 18',
   opp: 'at Seattle',
-  score: '',
+  score: '6:15',
   recap: ``
   },
   {
   id: 'apr19',
   date: 'April 19',
   opp: 'at Seattle',
-  score: '',
+  score: '3:10',
   recap: ``
   },
   {
   id: 'apr21',
   date: 'April 21',
   opp: 'Pittsburgh',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr22',
   date: 'April 22',
   opp: 'Pittsburgh',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr23',
   date: 'April 23',
   opp: 'Pittsburgh',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr24',
   date: 'April 24',
   opp: 'Athletics',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr25',
   date: 'April 25',
   opp: 'Athletics',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'apr26',
   date: 'April 26',
   opp: 'Athletics',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'apr27',
   date: 'April 27',
   opp: 'New York Yankees',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr28',
   date: 'April 28',
   opp: 'New York Yankees',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'apr29',
   date: 'April 29',
   opp: 'New York Yankees',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'may1',
   date: 'May1',
   opp: 'at Detroit',
-  score: '',
+  score: '5:40',
   recap: ``
   },
   {
   id: 'may2',
   date: 'May 2',
   opp: 'at Detroit',
-  score: '',
+  score: '6:15',
   recap: ``
   },
   {
   id: 'may3',
   date: 'May 3',
   opp: 'at Detroit',
-  score: '',
+  score: '12:40',
   recap: ``
   },
   {
   id: 'may5',
   date: 'May 5',
   opp: 'at New York Yankees',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'may6',
   date: 'May 6',
   opp: 'at New York Yankees',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'may7',
   date: 'May 7',
   opp: 'at New York Yankees',
-  score: '',
+  score: '11:35',
   recap: ``
   },
   {
   id: 'may8',
   date: 'May 8',
   opp: 'Chicago Cubs',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may9',
   date: 'May 9',
   opp: 'Chicago Cubs',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'may10',
   date: 'May 10',
   opp: 'Chicago Cubs',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'may11',
   date: 'May 11',
   opp: 'Arizona',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may12',
   date: 'May 12',
   opp: 'Arizona',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may13',
   date: 'May 13',
   opp: 'Arizona',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
@@ -325,154 +325,154 @@ const schedule = [
   id: 'may18',
   date: 'May 18',
   opp: 'at Colorado',
-  score: '',
+  score: '7:40',
   recap: ``
   },
   {
   id: 'may19',
   date: 'May 19',
   opp: 'at Colorado',
-  score: '',
+  score: '7:40',
   recap: ``
   },
   {
   id: 'may20',
   date: 'May 20',
   opp: 'at Colorado',
-  score: '',
+  score: '2:10',
   recap: ``
   },
   {
   id: 'may22',
   date: 'May 22',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '8:38',
   recap: ``
   },
   {
   id: 'may23',
   date: 'May 23',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '9:05',
   recap: ``
   },
   {
   id: 'may24',
   date: 'May 24',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '3:07',
   recap: ``
   },
   {
   id: 'may25',
   date: 'May 25',
   opp: 'Houston',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'may26',
   date: 'May 26',
   opp: 'Houston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may27',
   date: 'May 27',
   opp: 'Houston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may28',
   date: 'May 28',
   opp: 'Houston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may29',
   date: 'May 29',
   opp: 'Kansas City',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'may30',
   date: 'May 30',
   opp: 'Kansas City',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'may31',
   date: 'May 31',
   opp: 'Kansas City',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jun1',
   date: 'June 1',
   opp: 'at St. Louis',
-  score: '',
+  score: '6:45',
   recap: ``
   },
   {
   id: 'jun2',
   date: 'June 2',
   opp: 'at St. Louis',
-  score: '',
+  score: '6:45',
   recap: ``
   },
   {
   id: 'jun3',
   date: 'June 3',
   opp: 'at St. Louis',
-  score: '',
+  score: '6:45',
   recap: ``
   },
   {
   id: 'jun5',
   date: 'June 5',
   opp: 'Cleveland',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jun6',
   date: 'June 6',
   opp: 'Cleveland',
-  score: '',
+  score: '6:35',
   recap: ``
   },
   {
   id: 'jun7',
   date: 'June 7',
   opp: 'Cleveland',
-  score: '',
+  score: '1:05',
   recap: ``
   },
   {
   id: 'jun9',
   date: 'June 9',
   opp: 'at Kansas City',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'jun10',
   date: 'June 10',
   opp: 'at Kansas City',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'jun11',
   date: 'June 11',
   opp: 'at Kansas City',
-  score: '',
+  score: '1:10',
   recap: ``
   },
   {
@@ -500,63 +500,63 @@ const schedule = [
   id: 'jun15',
   date: 'June 15',
   opp: 'Minnesota',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jun16',
   date: 'June 16',
   opp: 'Minnesota',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jun18',
   date: 'June 18',
   opp: 'Minnesota',
-  score: '',
+  score: '12:35',
   recap: ``
   },
   {
   id: 'jun19',
   date: 'June 19',
   opp: 'San Diego',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jun20',
   date: 'June 20',
   opp: 'San Diego',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'jun21',
   date: 'June 21',
   opp: 'San Diego',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jun22',
   date: 'June 22',
   opp: 'at Miami',
-  score: '',
+  score: '5:40',
   recap: ``
   },
   {
   id: 'jun23',
   date: 'June 23',
   opp: 'at Miami',
-  score: '',
+  score: '5:40',
   recap: ``
   },
   {
   id: 'jun24',
   date: 'June 24',
   opp: 'at Miami',
-  score: '',
+  score: '11:10',
   recap: ``
   },
   {
@@ -612,154 +612,154 @@ const schedule = [
   id: 'jul2',
   date: 'July 2',
   opp: 'Detroit',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul4',
   date: 'July 4',
   opp: 'Detroit',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'jul5',
   date: 'July 5',
   opp: 'Detroit',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jul7',
   date: 'July 7',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul8',
   date: 'July 8',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul9',
   date: 'July 9',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul10',
   date: 'July 10',
   opp: 'Houston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul11',
   date: 'July 11',
   opp: 'Houston',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'jul12',
   date: 'July 12',
   opp: 'Houston',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jul17',
   date: 'July 17',
   opp: 'at Atlanta',
-  score: '',
+  score: '6:15',
   recap: ``
   },
   {
   id: 'jul18',
   date: 'July 18',
   opp: 'at Atlanta',
-  score: '',
+  score: '3:10',
   recap: ``
   },
   {
   id: 'jul19',
   date: 'July 19',
   opp: 'at Atlanta',
-  score: '',
+  score: '12:30',
   recap: ``
   },
   {
   id: 'jul20',
   date: 'July 20',
   opp: 'Chicago White Sox',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul21',
   date: 'July 21',
   opp: 'Chicago White Sox',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul22',
   date: 'July 22',
   opp: 'Chicago White Sox',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul24',
   date: 'July 24',
   opp: 'Seattle',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'jul25',
   date: 'July 25',
   opp: 'Seattle',
-  score: '',
+  score: '6:15',
   recap: ``
   },
   {
   id: 'jul26',
   date: 'July 26',
   opp: 'Seattle',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jul27',
   date: 'July 27',
   opp: 'Seattle',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'jul28',
   date: 'July 28',
   opp: 'at Tampa Bay',
-  score: '',
+  score: '5:40',
   recap: ``
   },
   {
   id: 'jul29',
   date: 'July 29',
   opp: 'at Tampa Bay',
-  score: '',
+  score: '5:40',
   recap: ``
   },
   {
   id: 'jul30',
   date: 'July 30',
   opp: 'at Tampa Bay',
-  score: '',
+  score: '11:10',
   recap: ``
   },
   {
@@ -787,350 +787,350 @@ const schedule = [
   id: 'aug3',
   date: 'August 3',
   opp: 'San Francisco',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug4',
   date: 'August 4',
   opp: 'San Francisco',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug5',
   date: 'August 5',
   opp: 'San Francisco',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug7',
   date: 'August 7',
   opp: 'Baltimore',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug8',
   date: 'August 8',
   opp: 'Baltimore',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'aug9',
   date: 'August 9',
   opp: 'Baltimore',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'aug10',
   date: 'August 10',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '8:38',
   recap: ``
   },
   {
   id: 'aug11',
   date: 'August 11',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '8:38',
   recap: ``
   },
   {
   id: 'aug12',
   date: 'August 12',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '8:38',
   recap: ``
   },
   {
   id: 'aug13',
   date: 'August 13',
   opp: 'at Los Angeles Angels',
-  score: '',
+  score: '9:07',
   recap: ``
   },
   {
   id: 'aug14',
   date: 'August 14',
   opp: 'at Athletics',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'aug15',
   date: 'August 15',
   opp: 'at Athletics',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'aug16',
   date: 'August 16',
   opp: 'at Athletics',
-  score: '',
+  score: '3:05',
   recap: ``
   },
   {
   id: 'aug18',
   date: 'August 18',
   opp: 'Washington',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug19',
   date: 'August 19',
   opp: 'Washington',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug20',
   date: 'August 20',
   opp: 'Washington',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug21',
   date: 'August 21',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'aug22',
   date: 'August 22',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'aug23',
   date: 'August 23',
   opp: 'Los Angeles Angels',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'aug24',
   date: 'August 24',
   opp: 'at Chicago White Sox',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'aug25',
   date: 'August 25',
   opp: 'at Chicago White Sox',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'aug26',
   date: 'August 26',
   opp: 'at Chicago White Sox',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'aug28',
   date: 'August 28',
   opp: 'at Milwwukee',
-  score: '',
+  score: '6:40',
   recap: ``
   },
   {
   id: 'aug29',
   date: 'August 29',
   opp: 'at Milwwukee',
-  score: '',
+  score: '6:15',
   recap: ``
   },
   {
   id: 'aug30',
   date: 'August 30',
   opp: 'at Milwwukee',
-  score: '',
+  score: '1:10',
   recap: ``
   },
   {
   id: 'aug31',
   date: 'August 31',
   opp: 'Athletics',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep1',
   date: 'September 1',
   opp: 'Athletics',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep2',
   date: 'September 2',
   opp: 'Athletics',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'sep3',
   date: 'September 3',
   opp: 'Tampa Bay',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep4',
   date: 'September 4',
   opp: 'Tampa Bay',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep5',
   date: 'September 5',
   opp: 'Tampa Bay',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'sep6',
   date: 'September 6',
   opp: 'Tampa Bay',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'sep8',
   date: 'September 8',
   opp: 'at Seattle',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'sep9',
   date: 'September 9',
   opp: 'at Seattle',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'sep10',
   date: 'September 10',
   opp: 'at Seattle',
-  score: '',
+  score: '3:10',
   recap: ``
   },
   {
   id: 'sep11',
   date: 'September 11',
   opp: 'at Arizona',
-  score: '',
+  score: '8:40',
   recap: ``
   },
   {
   id: 'sep12',
   date: 'September 12',
   opp: 'at Arizona',
-  score: '',
+  score: '7:10',
   recap: ``
   },
   {
   id: 'sep13',
   date: 'September 13',
   opp: 'at Arizona',
-  score: '',
+  score: '3:10',
   recap: ``
   },
   {
   id: 'sep15',
   date: 'September 15',
   opp: 'Boston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep16',
   date: 'September 16',
   opp: 'Boston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep17',
   date: 'September 17',
   opp: 'Boston',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep18',
   date: 'September 18',
   opp: 'Toronto',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep19',
   date: 'September 19',
   opp: 'Toronto',
-  score: '',
+  score: '6:05',
   recap: ``
   },
   {
   id: 'sep20',
   date: 'September 20',
   opp: 'Toronto',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'sep22',
   date: 'September 22',
   opp: 'New York Mets',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep23',
   date: 'September 23',
   opp: 'New York Mets',
-  score: '',
+  score: '7:05',
   recap: ``
   },
   {
   id: 'sep24',
   date: 'September 24',
   opp: 'New York Mets',
-  score: '',
+  score: '1:35',
   recap: ``
   },
   {
   id: 'sep25',
   date: 'September 25',
   opp: 'at Minnesota',
-  score: '',
+  score: '7:10',
   recap: ``
   },
   {
   id: 'sep26',
   date: 'September 26',
   opp: 'at Minnesota',
-  score: '',
+  score: '3:10',
   recap: ``
   },
   {
   id: 'sep27',
   date: 'September 27',
   opp: 'at Minnesota',
-  score: '',
+  score: '2:10',
   recap: ``
   },
 ]
