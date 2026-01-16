c3ae02c85caae9e6b00750174aee667f675daa8d
rwb-mp <icebluecamaro@gmail.com>
2026-01-16 12:12:46 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -2882,7 +2882,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("March 28 2026 14:35:00").getTime();
+const targetDate = new Date("March 26 2026 14:05:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
