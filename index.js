cc7368efa31b4878cc0490f23af89fa4d902f36d
rwb-mp <icebluecamaro@gmail.com>
2026-08-02 19:48:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1896,8 +1896,8 @@ document.getElementById("dlls").addEventListener("click", () => {
     date: '2',
     opp: 'at Hou <img src="logos/astros.gif" width="20px" height="20px" style="vertical-align:bottom">',
     time: '1:10',
-    score: '',
-    recap: 'RSN'
+    score: 'L 7-3',
+    recap: '<a href = "journal.html#aug2" target = "_blank">Recap</a>'
     },
     {
     id: '170',
@@ -2882,7 +2882,7 @@ const Hours = document.getElementById('hours');
 const Minutes = document.getElementById('minutes');
 const Seconds = document.getElementById('seconds');
 
-const targetDate = new Date("August 2 2026 13:10:00").getTime();
+const targetDate = new Date("August 3 2026 19:05:00").getTime();
 
 function timer () {
     const currentDate = new Date().getTime();
